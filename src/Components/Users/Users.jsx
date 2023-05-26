8592863f5f026361dfbb0f20e370bcba850b842f
NickiSchultz <nikolas200896@gmail.com>
2023-05-26 22:32:04 +0300

    I1

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,55 +1,75 @@
-import { NavLink} from 'react-router-dom';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 import userPhoto from './../../assets/images/icon-user-default.png'
-import styles from './Users.module.css';
+import styles from './Users.module.css'
 
+let Users = props => {
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
 
-let Users = (props) => {
+  let pages = []
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i)
+  }
 
-    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
-    }
-
-
-
-    return <div>
-        <div>
-            {pages.map(p => {
-                return <span className={props.currentPage === p && styles.selectedPage}
-                    onClick={(e) => { props.onPageChanged(p) }}>{p}</span>
-            })}
+  return (
+    <div>
+      <div>
+        {pages.map(p => {
+          return (
+            <button
+              key={p}
+              className={props.currentPage === p && styles.selectedPage}
+              onClick={() => {
+                props.onPageChanged(p)
+              }}
+            >
+              {p}
+            </button>
+          )
+        })}
+      </div>
+      {props.users.map(u => (
+        <div key={u.id}>
+          <span>
+            <div>
+              <NavLink to={'/profile/' + u.id}>
+                <img
+                  src={u.photos.small != null ? u.photos.small : userPhoto}
+                  alt=""
+                  className={styles.userPhoto}
+                />
+              </NavLink>
+            </div>
+            <div>
+              {u.followed ? (
+                <button
+                  disabled={props.followingInProgress.some(id => id === u.id)}
+                  onClick={() => {
+                    props.unFollow(u.id)
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  disabled={props.followingInProgress.some(id => id === u.id)}
+                  onClick={() => {
+                    props.follow(u.id)
+                  }}
+                >
+                  follow
+                </button>
+              )}
+            </div>
+          </span>
+          <span>
+            <div>{u.name}</div>
+            <div>{u.status}</div>
+          </span>
         </div>
-        {
-            props.users.map(u => <div key={u.id}>
-                <span>
-                    <div>
-                        <NavLink to={'/profile/' + u.id}>
-                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
-                        </NavLink>
-                    </div>
-                    <div>
-                        {u.followed
-                            ? <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                                props.unFollow(u.id);                           
-                            }}>Unfollow</button>
-
-                            : <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                                props.follow(u.id);                               
-                            }}>follow</button>}
-
-                    </div>
-                </span>
-                <span>
-
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-
-                </span>
-            </div>)
-        }
+      ))}
     </div>
+  )
 }
 
 export default Users

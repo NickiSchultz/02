15f109d35048d7bde4f05414d647c43afc0b412f
NickiSchultz <nikolas200896@gmail.com>
2023-05-26 22:16:31 +0300

    I

--- src/Components/Content/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Content/ProfileInfo/ProfileInfo.jsx
@@ -1,25 +1,25 @@
-import c from './ProfileInfo.module.css';
-import pan from './pan.jpg';
-import Preloader from '../../Common/Preloader/Preloader';
+import React from 'react'
+import c from './ProfileInfo.module.css'
+import pan from './pan.jpg'
+import Preloader from '../../Common/Preloader/Preloader'
 import ProfileStatus from './ProfileStatus.jsx'
 
-
-const ProfileInfo = (props) => {
-  if (!props.profile){
-    return <Preloader/>
+const ProfileInfo = props => {
+  if (!props.profile) {
+    return <Preloader />
   }
   return (
     <div className={c.content}>
       <div>
-        <img src={pan} alt=''/>
+        <img src={pan} alt="" />
       </div>
       <div className={c.discript}>
-        <img src={props.profile.photos.large} alt=''/>
-        <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
-        ava+port
+        <img src={props.profile.photos.large} alt="" />
+        <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+        ava+portglenfl
       </div>
     </div>
   )
 }
 
-export default ProfileInfo;
+export default ProfileInfo
